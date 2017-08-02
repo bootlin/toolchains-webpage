613d727f015ad64248b3d62b89bf43b3aebb1125
Florent Jacquet <florent.jacquet@free-electrons.com>
2017-08-02 11:41:22 +0200

    Refactor toolchains parsing with a dedicated class
    Signed-off-by: Florent Jacquet <florent.jacquet@free-electrons.com>

--- main.py
+++ main.py
@@ -9,17 +9,16 @@ import os
 import unicodedata
 import re
 import json
-import csv
 
 from collections import OrderedDict
 from datetime import datetime
 
 from jinja2 import FileSystemLoader, Environment
 
-import mistune
+from toolchain import Toolchain, ToolchainEncoder, ToolchainSet
 
 def to_json(value):
-    return json.dumps(value)
+    return json.dumps(value, cls=ToolchainEncoder)
 
 def slugify(value):
     """ Note: This was modified from django.utils.text slugify """
@@ -31,39 +30,18 @@ def slugify(value):
 
 jinja_env = Environment(loader=FileSystemLoader(os.getcwd()))
 jinja_env.filters['slugify'] = slugify
-jinja_env.filters['markdown'] = mistune.Markdown()
 jinja_env.filters['to_json'] = to_json
 
 TOOLCHAINS_DIR = "/srv/gitlabci/www/downloads"
 WWW_DIR = "/srv/gitlabci/www"
 
+
 def generate():
-    """
-    toolchains = {
-        "toolchain_name": <dict, toolchain_infos>
-        }
-    toolchains_tree = {
-        "arch": {
-            "libc": {
-                "version": <dict, toolchain_infos>
-                }
-            }
-        }
-    """
     start_time = datetime.now()
-    toolchains = {}
-    toolchains_tree = OrderedDict()
-    archs = {}
-    libcs = {}
-    versions = {}
+    toolchains = ToolchainSet()
     # Iterate over all releases
     for release in [e for e in os.scandir(TOOLCHAINS_DIR) if e.is_dir()]:
         toolchains_path = os.path.join(TOOLCHAINS_DIR, release.name)
-        toolchains[release.name] = {}
-        toolchains_tree[release.name] = OrderedDict()
-        archs[release.name] = set()
-        libcs[release.name] = set()
-        versions[release.name] = set()
         toolchain_list = []
         for a in os.scandir(os.path.join(toolchains_path, "toolchains")):
             try:
@@ -72,57 +50,22 @@ def generate():
                 pass
         # Iterate over all toolchains
         for toolchain in sorted([e for e in toolchain_list if e.is_file() and not e.name.startswith('.') and not e.name.endswith(".sha256")], key=lambda t: t.name):
-            toolchain_name = toolchain.name.split(".tar.")[0]
-            arch, libc, version = toolchain_name.split("--")
-            version = version.split('-20')[0]
-            archs[release.name].add(arch)
-            libcs[release.name].add(libc)
-            versions[release.name].add(version)
+            t = Toolchain(toolchain.name)
 
-            # Prepare the info dict
-            toolchain_infos = {
-                    'arch': arch,
-                    'libc': libc,
-                    'version': version,
-                    'name': toolchain_name,
-                    }
-            with open(os.path.join(toolchains_path, "toolchains", arch, "readmes", toolchain_name + ".txt")) as f:
-                toolchain_infos['manifest'] = f.read()
-            flag = re.search(r"FLAG: (\S*)", toolchain_infos['manifest'])
-            toolchain_infos['flag'] = flag.group(1)
-            toolchain_infos['manifest'] = '\n'.join(toolchain_infos['manifest'].split('\n')[2:-2])
+            with open(os.path.join(toolchains_path, "toolchains", t.arch,
+                "readmes", t.name + ".txt")) as f:
+                t.set_manifest(f)
 
-            summary_list = ['gdb', 'gcc-final', 'linux', 'uclibc', 'musl', 'glibc', 'binutils']
-            found_list = []
-            toolchain_infos['summary'] = []
-            with open(os.path.join(toolchains_path, "toolchains", arch, "summaries", toolchain_name + ".csv")) as f:
-                summary = csv.reader(f, delimiter=",", quotechar='"')
-                for row in summary:
-                    if any(e in row[0] for e in summary_list if e not in found_list):
-                        row[0] = row[0].replace("gcc-final", "gcc")
-                        toolchain_infos['summary'].append([row[0], row[1]])
-                        found_list.append(row[0])
-            toolchain_infos['summary'] = sorted(toolchain_infos['summary'], key=lambda e: e[0])
+            with open(os.path.join(toolchains_path, "toolchains", t.arch,
+                "summaries", t.name + ".csv")) as f:
+                t.set_summary(f)
 
-            # Build the two dicts: the raw list and the tree
-            toolchains[release.name][toolchain_name] = toolchain_infos
-            if arch not in toolchains_tree[release.name].keys():
-                toolchains_tree[release.name][arch] = OrderedDict()
-            if libc not in toolchains_tree[release.name][arch].keys():
-                toolchains_tree[release.name][arch][libc] = OrderedDict()
-            if version not in toolchains_tree[release.name][arch][libc].keys():
-                toolchains_tree[release.name][arch][libc][version] = []
-
-            toolchains_tree[release.name][arch][libc][version].append(toolchain_infos)
+            toolchains.add(release.name, t)
 
     for p in ['index', 'status', 'faq', 'changelog']:
         template = jinja_env.get_template("templates/%s.jinja" % p)
         html = template.render(
                 toolchains=toolchains,
-                toolchains_tree=toolchains_tree,
-                archs=archs,
-                libcs=libcs,
-                versions=versions,
                 datetime=datetime,
                 start_time=start_time
                 )
---
+++ toolchain.py
@@ -0,0 +1,105 @@
+import json
+import csv
+import re
+
+from collections import OrderedDict
+
+class Toolchain(object):
+    def __init__(self, file_name):
+        toolchain_name = file_name.split(".tar.")[0]
+        arch, libc, version = toolchain_name.split("--")
+        version = version.split('-20')[0]
+        self.arch = arch
+        self.libc = libc
+        self.version = version
+        self.name = toolchain_name
+
+    def set_manifest(self, f):
+        manifest = f.read()
+        flag = re.search(r"FLAG: (\S*)", manifest)
+        self.flag = flag.group(1)
+        self.manifest = '\n'.join(manifest.split('\n')[2:-2])
+
+    def set_summary(self, f):
+        summary_list = ['gdb', 'gcc-final', 'linux', 'uclibc', 'musl', 'glibc', 'binutils']
+        found_list = []
+        summary = []
+        s = csv.reader(f, delimiter=",", quotechar='"')
+        for row in s:
+            if any(e in row[0] for e in summary_list if e not in found_list):
+                row[0] = row[0].replace("gcc-final", "gcc")
+                summary.append([row[0], row[1]])
+                found_list.append(row[0])
+        self.summary = sorted(summary, key=lambda e: e[0])
+
+    def as_dict(self):
+        return {
+                'name': self.name,
+                'arch': self.arch,
+                'libc': self.libc,
+                'version': self.version,
+                'flag': self.flag,
+                'summary': self.summary,
+                'manifest': self.manifest,
+                }
+
+class ToolchainEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Toolchain):
+            return obj.as_dict()
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+class ToolchainSet(object):
+    def __init__(self):
+        self.archs = {}
+        self.libcs = {}
+        self.versions = {}
+        self.tree = OrderedDict()
+        self.list_by_release = {}
+
+    def add(self, release, t):
+        if release not in self.list_by_release:
+            self.list_by_release[release] = {}
+
+        if release not in self.tree:
+            self.tree[release] = OrderedDict()
+
+        if release not in self.archs:
+            self.archs[release] = set()
+
+        if release not in self.libcs:
+            self.libcs[release] = set()
+
+        if release not in self.versions:
+            self.versions[release] = set()
+
+        self.archs[release].add(t.arch)
+        self.libcs[release].add(t.libc)
+        self.versions[release].add(t.version)
+
+        if t.arch not in self.tree[release]:
+            self.tree[release][t.arch] = OrderedDict()
+        if t.libc not in self.tree[release][t.arch]:
+            self.tree[release][t.arch][t.libc] = OrderedDict()
+        if t.version not in self.tree[release][t.arch][t.libc]:
+            self.tree[release][t.arch][t.libc][t.version] = []
+
+        self.tree[release][t.arch][t.libc][t.version].append(t)
+        self.list_by_release[release][t.name] = t
+
+    def arch_list(self, release):
+        return sorted(list(self.archs[release]))
+
+    def libc_list(self, release):
+        return sorted(list(self.libcs[release]))
+
+    def version_list(self, release):
+        return sorted(list(self.libcs[release]))
+
+    def release_list(self):
+        return sorted(list(self.tree.keys()))
+
+    def get_tree(self, release):
+        return self.tree[release]
+
